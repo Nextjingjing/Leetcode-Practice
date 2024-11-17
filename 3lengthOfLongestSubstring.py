cbcd9f14a0c13981e14c505e5309bb3ec99b8d22
SNeXT <snext2004@gmail.com>
2024-11-17 19:26:41 +0700

    3lengthOfLongestSubstring

---
+++ 3lengthOfLongestSubstring.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        maxLen = 0
+        for mainIndex in range(len(s)):
+            unrepeatedString = ""
+            for index in range(mainIndex, len(s)):
+                char = s[index]
+                if index == mainIndex:
+                    unrepeatedString += char
+                elif unrepeatedString.find(char) == -1:
+                    unrepeatedString += char        
+                else:
+                    break
+                
+            if len(unrepeatedString) > maxLen:
+                print(unrepeatedString)
+                maxLen = len(unrepeatedString)
+        
+        return maxLen
